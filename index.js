ef8ae769564a25b5521eda0f9c3652b6e9195dac
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-19 11:30:24 +0200

    created framework route

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ import express from 'express';
 const app = express();
 const port = 4588;
 
+const frameworks = [
+	{
+		title: "React",
+		url: "https://react.dev"
+	},
+	{
+		title: "Next.js",
+		url: "https://nextjs.org"
+	},
+	{
+		title: "SvelteKit",
+		url: "https://kit.svelte.dev"
+	}
+]
+
 app.get('/', (req, res) => {
 	res.send('Fullstack API')
 })
 
-// app.get('/frameworks', (req, res) => {
-// 	res.
-// })
+app.get('/frameworks', (req, res) => {
+	res.json(frameworks);
+})
 
 app.listen(port, () => {
 	console.log(`listening on port http://localhost:${port}`);
